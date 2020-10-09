9060fad75669fb1735c6afb5b9f85c36fdb8cd16
AnettKeszler <anett.dr.keszler@gmail.com>
2020-10-09 10:55:51 +0200

    add series entity and create series psql table

---
+++ src/main/java/com/codecool/jpaseriesapp/JpaSeriesAppApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.jpaseriesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSeriesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSeriesAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codecool/jpaseriesapp/entity/Season.java
@@ -0,0 +1,4 @@
+package com.codecool.jpaseriesapp.entity;
+
+public class Season {
+}
