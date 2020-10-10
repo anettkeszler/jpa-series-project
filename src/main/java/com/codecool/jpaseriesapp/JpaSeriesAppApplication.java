0c8ce5ad9aa1c2479dfb46d391170553b0fc5a00
AnettKeszler <anett.dr.keszler@gmail.com>
2020-10-10 23:49:26 +0200

    add all data to tables

--- src/main/java/com/codecool/jpaseriesapp/JpaSeriesAppApplication.java
+++ src/main/java/com/codecool/jpaseriesapp/JpaSeriesAppApplication.java
@@ -45,17 +45,158 @@ public class JpaSeriesAppApplication {
                     .title("My Dripping Sleep")
                     .build();
 
+            Episode episode4 = Episode.builder()
+                    .title("Tonight We Improvise")
+                    .build();
+
+            Episode episode5 = Episode.builder()
+                    .title("Ruling Days")
+                    .build();
+
+            Episode episode6 = Episode.builder()
+                    .title("Book of Ruth")
+                    .build();
+
+            Episode episode7 = Episode.builder()
+                    .title("Nest Box"	)
+                    .build();
+
+            Episode episode8 = Episode.builder()
+                    .title("Kaleidoscope")
+                    .build();
+
+            Episode episode9 = Episode.builder()
+                    .title("Coffee, Black")
+                    .build();
+
+            Episode episode10 = Episode.builder()
+                    .title("The Toll")
+                    .build();
+
+            Episode episode11 = Episode.builder()
+                    .title("Reparations")
+                    .build();
+
+            Episode episode12 = Episode.builder()
+                    .title("The Precious Blood of Jesus")
+                    .build();
+
+            Episode episode13 = Episode.builder()
+                    .title("Once a Langmore...")
+                    .build();
+
+            Episode episode14 = Episode.builder()
+                    .title("Stag")
+                    .build();
+
+            Episode episode15 = Episode.builder()
+                    .title("Game Day")
+                    .build();
+
+            Episode episode16 = Episode.builder()
+                    .title("Outer Darkness")
+                    .build();
+
+            Episode episode17 = Episode.builder()
+                    .title("One Way Out")
+                    .build();
+
+            Episode episode18 = Episode.builder()
+                    .title("The Big Sleep")
+                    .build();
+
+            Episode episode19 = Episode.builder()
+                    .title("The Badger")
+                    .build();
+
+            Episode episode20 = Episode.builder()
+                    .title("The Gold Coast")
+                    .build();
+
+            Episode episode21 = Episode.builder()
+                    .title("Wartime")
+                    .build();
+
+            Episode episode22 = Episode.builder()
+                    .title("Civil Union")
+                    .build();
+
+            Episode episode23 = Episode.builder()
+                    .title("Kevin Cronin Was Here")
+                    .build();
+
+            Episode episode24 = Episode.builder()
+                    .title("Boss Fight")
+                    .build();
+
+            Episode episode25 = Episode.builder()
+                    .title("It Came From Michoacán")
+                    .build();
+
+            Episode episode26 = Episode.builder()
+                    .title("Su Casa Es Mi Casa")
+                    .build();
+
+            Episode episode27 = Episode.builder()
+                    .title("In Case of Emergency")
+                    .build();
+
+            Episode episode28 = Episode.builder()
+                    .title("BFF")
+                    .build();
+
+            Episode episode29 = Episode.builder()
+                    .title("Fire Pink")
+                    .build();
+
+            Episode episode30 = Episode.builder()
+                    .title("All in")
+                    .build();
+
+
             Season season1 = Season.builder()
                     .numberOfEpisodes(10)
                     .seasonNumber(1)
                     .episode(episode1)
                     .episode(episode2)
+                    .episode(episode3)
+                    .episode(episode4)
+                    .episode(episode5)
+                    .episode(episode6)
+                    .episode(episode7)
+                    .episode(episode8)
+                    .episode(episode9)
+                    .episode(episode10)
                     .build();
 
             Season season2 = Season.builder()
                     .numberOfEpisodes(10)
                     .seasonNumber(2)
-                    .episode(episode3)
+                    .episode(episode11)
+                    .episode(episode12)
+                    .episode(episode13)
+                    .episode(episode14)
+                    .episode(episode15)
+                    .episode(episode16)
+                    .episode(episode17)
+                    .episode(episode18)
+                    .episode(episode19)
+                    .episode(episode20)
+                    .build();
+
+            Season season3 = Season.builder()
+                    .numberOfEpisodes(10)
+                    .seasonNumber(3)
+                    .episode(episode21)
+                    .episode(episode22)
+                    .episode(episode23)
+                    .episode(episode24)
+                    .episode(episode25)
+                    .episode(episode26)
+                    .episode(episode27)
+                    .episode(episode28)
+                    .episode(episode29)
+                    .episode(episode30)
                     .build();
 
             Series ozark = Series.builder()
@@ -64,20 +205,48 @@ public class JpaSeriesAppApplication {
                     .countryOfOrigin("USA")
                     .season(season1)
                     .season(season2)
+                    .season(season3)
                     .numberOfSeasons(3)
                     .numberOfEpisodes(30)
                     .releaseYear(LocalDate.of(2017, 07, 21))
                     .build();
 
-//            ozark.calculateAge();
-
             season1.setSeries(ozark);
             season2.setSeries(ozark);
+            season3.setSeries(ozark);
 
             episode1.setSeason(season1);
             episode2.setSeason(season1);
+            episode3.setSeason(season1);
+            episode4.setSeason(season1);
+            episode5.setSeason(season1);
+            episode6.setSeason(season1);
+            episode7.setSeason(season1);
+            episode8.setSeason(season1);
+            episode9.setSeason(season1);
+            episode10.setSeason(season1);
 
-            episode3.setSeason(season2);
+            episode11.setSeason(season2);
+            episode12.setSeason(season2);
+            episode13.setSeason(season2);
+            episode14.setSeason(season2);
+            episode15.setSeason(season2);
+            episode16.setSeason(season2);
+            episode17.setSeason(season2);
+            episode18.setSeason(season2);
+            episode19.setSeason(season2);
+            episode20.setSeason(season2);
+
+            episode21.setSeason(season3);
+            episode22.setSeason(season3);
+            episode23.setSeason(season3);
+            episode24.setSeason(season3);
+            episode25.setSeason(season3);
+            episode26.setSeason(season3);
+            episode27.setSeason(season3);
+            episode28.setSeason(season3);
+            episode29.setSeason(season3);
+            episode30.setSeason(season3);
 
             seriesRepository.save(ozark);
         };
